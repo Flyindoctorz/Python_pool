817e66b432def59991bfc20718abb9362c75b64d
cgelgon <cgelgon@f6r12s14.paris.42.school>
2026-05-13 11:34:59 +0200

    ex08

--- 00/ex08/Loading.py
+++ 00/ex08/Loading.py
@@ -2,9 +2,15 @@ import os
 
 
 def ft_tqdm(lst: range) -> None:
+    total = len(lst)
     termin_width = os.get_terminal_size().columns
     for i, elem in enumerate(lst):
+        percent = i / total * 100
+        size = len(f"{percent:.0f}%|") + len(f"| {total}/{total}") + 3
+        toolbar_width = termin_width - size
         i += 1
-        total = len(lst)
-        percent = i / total *100
+        filled = int(percent * toolbar_width / 100)
+        tofill = toolbar_width - filled
+        bar = "[" + "=" * filled + ">" + " " * tofill + "]"
+        print(f"{percent:.0f}%|{bar}| {i}/{total}", end="\r", flush=True)
         yield elem
